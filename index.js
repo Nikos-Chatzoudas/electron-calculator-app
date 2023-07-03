4ed5de9fbb06475ff9d9f90c634ac351014a926f
Nikos Chatzoudas <126014069+Nikos-Chatzoudas@users.noreply.github.com>
2023-07-03 13:34:59 +0300

    main

---
+++ index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 346,
+    height: 612   ,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    resizable:false
+  });
+
+  win.loadFile('index.html');
+}
+app.whenReady().then(createWindow);

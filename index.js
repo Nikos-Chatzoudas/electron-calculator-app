4664ec2fa826e79847b4a1377db6f71d4c9d126c
Nikos Chatzoudas <126014069+Nikos-Chatzoudas@users.noreply.github.com>
2023-07-03 13:49:28 +0300

    size

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron');
 function createWindow() {
   const win = new BrowserWindow({
     width: 346,
-    height: 612   ,
+    height: 632,
     autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
